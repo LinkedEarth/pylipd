27953041de2d73060186f88860f0574a695653a9
Varun Ratnakar <varunratnakar@gmail.com>
2023-02-10 23:33:43 +0530

    readthedocs dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -260,10 +260,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'Pylipd.tex', 'Pylipd Documentation',
-     author, 'manual'),
-]
+#latex_documents = [
+#    (master_doc, 'Pylipd.tex', 'Pylipd Documentation',
+#     author, 'manual'),
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -304,11 +304,11 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'Pylipd', 'Pylipd Documentation',
-     author, 'Pylipd', 'One line description of project.',
-     'Miscellaneous'),
-]
+#texinfo_documents = [
+#    (master_doc, 'Pylipd', 'Pylipd Documentation',
+#     author, 'Pylipd', 'One line description of project.',
+#     'Miscellaneous'),
+#]
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
