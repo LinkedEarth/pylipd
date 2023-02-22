d621986616cd4cf1f06a430f4927449b29f8dbd0
Deborah Khider <khider@usc.edu>
2023-02-21 18:12:12 -0800

    Update test_LiPD.py

--- pylipd/tests/test_LiPD.py
+++ pylipd/tests/test_LiPD.py
@@ -47,9 +47,9 @@ class TestLiPDLoad():
             #print(lipd_remote.get_all_dataset_names())
     
 
-class TestLiPDids():
+class TestLiPDnames():
     
-    def test_get_all_dataset_ids_t0(self):
+    def test_get_all_dataset_names_t0(self):
         url = ['https://lipdverse.org/data/RRh3T4NCsf4MgrxhXbJq/1_0_0//Ocn-Philippines.Stott.2007.lpd',
                'https://lipdverse.org/data/LCd0404b13039620e9ec2b82dbdcf87861/1_0_1//LedovyiObryvExposureNorthernSection.Anderson.2002.lpd']
         
