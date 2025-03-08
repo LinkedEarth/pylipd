3a2e9ebc18ec7445472bea7e419ff2a498dc14fc
Varun Ratnakar <varunratnakar@gmail.com>
2025-03-08 15:27:33 +0530

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.3.7.b3'
+version = '1.3.7.b4'
 
 # Read the readme file contents into variable
 def read(fname):
@@ -35,5 +35,5 @@ setup(
         "numpy",
         "bibtexparser"
     ],
-    python_requires=">=3.11.0"
+    python_requires=">=3.10.0"
 )
